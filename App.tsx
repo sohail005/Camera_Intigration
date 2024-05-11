b27ffd6c40ead4f211de5119f9c39f97ffce2315
SOHAIL <sohail345patel@gmail.com>
2024-05-11 11:08:23 +0530

    test

--- App.tsx
+++ App.tsx
@@ -33,15 +33,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
+      
       <Text
         style={[
           styles.sectionDescription,
